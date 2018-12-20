4b40d2d961023dfa5e20f3020477ee37d5693dde
Chandni Patel <chandnir.patel@gmail.com>
2018-12-20 14:43:58 +0100

    Initial comment that contains the basic project setup and instructions from Leetcode

---
+++ src/main/scala/TwoSum.scala
@@ -0,0 +1,16 @@
+class TwoSum {
+/* This problem is from Leetcode
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You are given an object named Solution with a method named twoSum.
+Example:
+
+Given nums = [2, 7, 11, 15], target = 9,
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+
+ */
+
+}
